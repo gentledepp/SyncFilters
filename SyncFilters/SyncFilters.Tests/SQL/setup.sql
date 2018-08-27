871d08111c955b45fe619830253d270c39a16757
alexander.marek <alexander.marek@opti-q.com>
2018-08-27 14:39:26 +0200

    Included sql files

---
+++ SyncFilters/SyncFilters.Tests/SQL/setup.sql
@@ -0,0 +1,251 @@
+﻿CREATE TABLE [dbo].[PrincipalEdge](
+	[StartVertex] [bigint] NOT NULL,
+	[EndVertex] [bigint] NOT NULL,
+	[Hops] [int] NOT NULL,
+	[StartPrincipalType] [tinyint] NOT NULL,
+	[EndPrincipalType] [tinyint] NOT NULL,
+	[DelMark] [bit] NOT NULL DEFAULT 0,
+	[TenantId] [int] NOT NULL,
+	[Modified] [timestamp] NOT NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0,
+ CONSTRAINT [PK_PrincipalEdge] PRIMARY KEY CLUSTERED 
+(
+	[StartVertex] ASC,
+	[EndVertex] ASC,
+	[Hops] ASC,
+	[StartPrincipalType] ASC,
+	[EndPrincipalType] ASC,
+	[TenantId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+GO
+
+
+CREATE PROCEDURE [dbo].[AddEdgeWithSpaceSavingsPrincipal]
+    @StartVertexId [bigint],
+    @EndVertexId [bigint],
+    @StartPrincipalType [tinyint] = 0,
+    @EndPrincipalType [tinyint] = 0,
+    @TenantId [int]
+AS
+BEGIN
+    SET NOCOUNT ON
+    IF EXISTS(SELECT Hops 
+    FROM PrincipalEdge 
+    WHERE StartVertex = @StartVertexId 
+    AND EndVertex = @EndVertexId 
+    AND StartPrincipalType = @StartPrincipalType
+    AND EndPrincipalType = @EndPrincipalType
+    AND TenantId = @TenantId
+    AND Hops = 0)
+    BEGIN
+    RETURN 0 -- DO NOTHING!!!
+    END
+    
+    IF @StartVertexId = @EndVertexId AND @StartPrincipalType = @EndPrincipalType
+    OR EXISTS (SELECT Hops 
+    FROM PrincipalEdge
+    WHERE StartVertex = @EndVertexId 
+    AND EndVertex = @StartVertexId
+    AND StartPrincipalType = @EndPrincipalType
+    AND EndPrincipalType = @StartPrincipalType
+    AND TenantId = @TenantId)
+    BEGIN
+    RAISERROR ('Attempt to create a circular relation detected!', 16, 1)
+    RETURN 0
+    END
+    
+    CREATE TABLE #CandidatesPrincipalEdge ( 
+    StartVertex bigint,
+    EndVertex bigint,
+    StartPrincipalType tinyint,
+    EndPrincipalType tinyint,
+    TenantId bigint)
+    
+    -- step 1: A's incoming edges to B
+    INSERT INTO #CandidatesPrincipalEdge
+    SELECT StartVertex 
+    , @EndVertexId
+    , StartPrincipalType
+    , @EndPrincipalType
+    , TenantId
+    FROM PrincipalEdge
+    WHERE EndVertex = @StartVertexId
+    AND EndPrincipalType = @StartPrincipalType
+    AND TenantId = @TenantId
+    
+    -- step 2: A to B's outgoing edges
+    UNION
+    SELECT @StartVertexId
+    , EndVertex
+    , @StartPrincipalType
+    , EndPrincipalType
+    , TenantId
+    FROM PrincipalEdge
+    WHERE StartVertex = @EndVertexId
+    AND StartPrincipalType = @EndPrincipalType
+    AND TenantId = @TenantId
+    
+    -- step 3: A’s incoming edges to end vertex of B's outgoing edges
+    UNION
+    SELECT A.StartVertex 
+    , B.EndVertex
+    , A.StartPrincipalType
+    , B.EndPrincipalType
+    , A.TenantId
+    FROM PrincipalEdge A
+    CROSS JOIN PrincipalEdge B
+    WHERE A.EndVertex = @StartVertexId
+    AND B.StartVertex = @EndVertexId
+    AND A.EndPrincipalType = @StartPrincipalType
+    AND B.StartPrincipalType = @EndPrincipalType
+    AND A.TenantId = B.TenantId
+    
+    INSERT INTO PrincipalEdge (
+    StartVertex,
+    EndVertex,
+    Hops,
+    StartPrincipalType,
+    EndPrincipalType,
+    TenantId)
+    VALUES ( 
+    @StartVertexId
+    , @EndVertexId
+    , 0
+    , @StartPrincipalType
+    , @EndPrincipalType
+    , @TenantId)
+
+   update  E
+   set --E.Modified = DEFAULT, not necessary as timestamp/rowversion rows are updated anyway
+       E.IsDeleted = 0 -- remove deletion flag 
+   from PRINCIPALEDGE E inner join #CandidatesPrincipalEdge C on E.StartVertex = C.StartVertex
+                     AND E.EndVertex = C.EndVertex 
+                     AND E.Hops = 1
+    
+    INSERT INTO PrincipalEdge (
+    StartVertex,
+    EndVertex,
+    Hops,
+    StartPrincipalType,
+    EndPrincipalType,
+    TenantId)
+    SELECT StartVertex,
+    EndVertex,
+    1,
+    StartPrincipalType,
+    EndPrincipalType,
+    TenantId
+    FROM #CandidatesPrincipalEdge C
+    WHERE NOT EXISTS (
+    SELECT Hops
+    FROM PrincipalEdge E
+    WHERE E.StartVertex = C.StartVertex
+    AND E.EndVertex = C.EndVertex 
+    AND E.Hops = 1
+    AND E.StartPrincipalType = C.StartPrincipalType
+    AND E.EndPrincipalType = C.EndPrincipalType
+    And E.TenantId = C.TenantId)
+    
+END
+
+
+GO
+
+
+
+CREATE PROCEDURE [dbo].[RemoveEdgeWithSpaceSavingsPrincipal]
+    @StartVertexId [bigint],
+    @EndVertexId [bigint],
+    @PrincipalType [tinyint] = 1,
+    @TenantId [int]
+AS
+BEGIN
+    
+    SET NOCOUNT ON
+    Update PrincipalEdge
+    set IsDeleted = 1
+	WHERE Hops = 0
+    AND StartVertex = @StartVertexId
+    AND EndVertex = @EndVertexId
+    AND StartPrincipalType = @PrincipalType
+    AND TenantId = @TenantId
+    
+    IF @@ROWCOUNT = 0
+    BEGIN
+    RETURN -- NOTHING TO DELETE
+    END
+    
+    --UPDATE PrincipalEdge SET DelMark = 0
+    
+    UPDATE PrincipalEdge
+    SET DelMark = 1
+    FROM PrincipalEdge 
+    INNER JOIN (
+    SELECT StartVertex
+    , @EndVertexId AS EndVertex
+    , StartPrincipalType as StartPrincipalType
+    FROM PrincipalEdge
+    WHERE EndVertex = @StartVertexId
+    AND StartPrincipalType = @PrincipalType
+    AND TenantId = @TenantId
+    UNION
+    SELECT @StartVertexId
+    , EndVertex AS EndVertex
+    , StartPrincipalType as StartPrincipalType
+    FROM PrincipalEdge
+    WHERE StartVertex = @EndVertexId
+    AND StartPrincipalType = @PrincipalType
+    AND TenantId = @TenantId
+    UNION
+    SELECT A.StartVertex 
+    , B.EndVertex
+    , A.StartPrincipalType as StartPrincipalType
+    FROM PrincipalEdge A
+    CROSS JOIN PrincipalEdge B
+    WHERE A.EndVertex = @StartVertexId
+    AND B.StartVertex = @EndVertexId
+    AND A.StartPrincipalType = @PrincipalType
+    AND A.TenantId = @TenantId
+    AND B.TenantId = @TenantId
+    ) AS C
+    ON C.StartVertex = PrincipalEdge.StartVertex
+    AND C.EndVertex = PrincipalEdge.EndVertex 
+    AND c.StartPrincipalType = PrincipalEdge.StartPrincipalType
+    WHERE Hops > 0
+	and IsDeleted = 0;
+    
+    WITH SafeRows AS
+    ( SELECT StartVertex, EndVertex, StartPrincipalType FROM PrincipalEdge WHERE DelMark = 0 and IsDeleted = 0 AND TenantId = @TenantId)
+    UPDATE PrincipalEdge
+    SET DelMark = 0
+    FROM PrincipalEdge
+    INNER JOIN SafeRows S1
+    ON S1.StartVertex = PrincipalEdge.StartVertex
+    INNER JOIN SafeRows S2 
+    ON S1.EndVertex = S2.StartVertex
+    AND S2.EndVertex = PrincipalEdge.EndVertex
+    WHERE DelMark = 1
+	   and IsDeleted = 0;
+    
+    WITH SafeRows AS
+    ( SELECT StartVertex, EndVertex FROM PrincipalEdge WHERE DelMark = 0 and IsDeleted = 0  AND TenantId = @TenantId)
+    UPDATE PrincipalEdge
+    SET DelMark = 0
+    FROM PrincipalEdge 
+    INNER JOIN SafeRows	S1
+    ON S1.StartVertex = PrincipalEdge.StartVertex
+    INNER JOIN SafeRows S2 
+    ON S1.EndVertex = S2.StartVertex
+    INNER JOIN SafeRows S3    
+    ON S2.EndVertex = S3.StartVertex
+    AND S3.EndVertex = PrincipalEdge.EndVertex
+    WHERE DelMark = 1
+	   and IsDeleted   = 0;
+    
+   Update PrincipalEdge 
+		set delmark = 0, IsDeleted = 1--, Modified = getutcdate() not necessary as timestamp/rowversion columns are updated anyways
+        WHERE DelMark = 1 
+END
---
+++ SyncFilters/SyncFilters.Tests/SQL/syncfilter.sql
@@ -0,0 +1,195 @@
+﻿
+CREATE TABLE [dbo].[__TARGETTABLE___syncfilter](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[ItemId] [uniqueidentifier] NOT NULL,
+	[Modified] [binary](8) NOT NULL,
+	[AppliedRules] [int] NOT NULL,
+	[UserId] [bigint] NOT NULL,
+	[TenantId] [bigint] NOT NULL,
+	[DeletionTime] [datetime2](7) NOT NULL DEFAULT '1753-01-01',
+	[RowVersion] [timestamp] NOT NULL,
+ CONSTRAINT [PK___TARGETTABLE___syncfilter] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+GO
+
+
+
+ALTER PROCEDURE [dbo].[__TARGETTABLE___TenantIdUserId__selectchanges]
+	@sync_min_timestamp bigint,
+	@sync_scope_id uniqueidentifier,
+	@sync_scope_is_new bit,
+	@sync_scope_is_reinit bit,
+	@TenantId bigint,
+	@UserId bigint
+AS
+BEGIN
+
+-- clause 1: get all modified items where there exists at least one non-deleted sync row
+SELECT 	[side].[ID], 
+	[base].[Title], 
+	[base].[Description], 
+	[base].[StatusValue], 
+	[base].[EscalationLevel], 
+	[base].[Opened], 
+	[base].[Closed], 
+	[base].[TenantId], 
+	[side].[sync_row_is_tombstone], 
+	[side].[create_scope_id], 
+	[side].[create_timestamp], -- keep the timestamp, as in the 1st clause, we focus on the changed items (not added sync rows)
+	[side].[update_scope_id], 
+	[side].[update_timestamp]  
+FROM [__TARGETTABLE__] [base]
+RIGHT JOIN [__TARGETTABLE___tracking] [side]
+ON [base].[Id] = [side].[Id]
+WHERE (
+	([side].[TenantId] = @TenantId)
+	OR (([side].[update_scope_id] = @sync_scope_id or [side].[update_scope_id] IS NULL)
+		AND ([side].[TenantId] IS NULL))
+	)
+AND (
+	-- Update made by the local instance
+	[side].[update_scope_id] IS NULL
+	-- Or Update different from remote
+	OR [side].[update_scope_id] <> @sync_scope_id
+	-- Or we are in reinit mode so we take rows even thoses updated by the scope
+	OR @sync_scope_is_reinit = 1
+    )
+AND (
+	-- And Timestamp is > from remote timestamp
+	[side].[timestamp] > @sync_min_timestamp
+	OR
+	-- remote instance is new, so we don't take the last timestamp
+	@sync_scope_is_new = 1
+	)
+AND (
+	-- And the row is toombstoned
+	[side].[sync_row_is_tombstone] = 1 
+	OR
+	(
+		-- Or there exists a row in the base table (RIGHT JOIN!)s
+		[side].[sync_row_is_tombstone] = 0 AND [base].[Id] is not null	
+		-- and there exists at least one non-deleted syncfilter
+		and exists (select s.id from __TARGETTABLE___syncfilter s
+						inner join principaledge e on e.endvertex = s.userid and e.startprincipaltype = 1 --user
+						where s.itemid = [base].id and e.startvertex = @userid and s.appliedrules > 0 and e.isdeleted = 0)
+	)
+)
+UNION ALL
+-- clause 2: get all items, which were not modified, but their sync rows are newer (toombstoned or not)
+SELECT 	[side].[ID], 
+	[base].[Title], 
+	[base].[Description], 
+	[base].[StatusValue], 
+	[base].[EscalationLevel], 
+	[base].[Opened], 
+	[base].[Closed], 
+	[base].[TenantId], 
+	[side].[sync_row_is_tombstone], 
+	[side].[create_scope_id], 
+	--[side].[create_timestamp], 
+	@sync_min_timestamp as [create_timestamp], -- use the clients timestamp as "create", so dotmim.sync registers any new rows that are added due to added sync filters as "DmRowState.Added"!
+	[side].[update_scope_id], 
+	[side].[update_timestamp]  
+FROM [__TARGETTABLE__] [base]
+RIGHT JOIN [__TARGETTABLE___tracking] [side]
+ON [base].[Id] = [side].[Id]
+WHERE (
+	([side].[TenantId] = @TenantId)
+	OR (([side].[update_scope_id] = @sync_scope_id or [side].[update_scope_id] IS NULL)
+		AND ([side].[TenantId] IS NULL))
+	)
+-- we do NOT care who actually made the changes, as the sync is triggered by the syncfilter table only!
+--AND (
+--	-- Update made by the local instance
+--	[side].[update_scope_id] IS NULL
+--	-- Or Update different from remote
+--	OR [side].[update_scope_id] <> @sync_scope_id
+--	-- Or we are in reinit mode so we take rows even thoses updated by the scope
+--	OR @sync_scope_is_reinit = 1
+--    )
+AND (
+	-- And Timestamp is <= from remote timestamp, as the items that DO have changes are already included in clause 1!
+	[side].[timestamp] <= @sync_min_timestamp
+	)
+-- and there is at least one younger, non-deleted syncfilter (permission was added to user)
+and exists (select s.id from __TARGETTABLE___syncfilter s
+					inner join principaledge e on e.endvertex = s.userid and e.startprincipaltype = 1 --user
+						where s.itemid = [base].id and e.startvertex = @userid and s.appliedrules > 0 and e.IsDeleted = 0 and (s.modified >  @sync_min_timestamp or e.modified > @sync_min_timestamp))
+-- make sure this item was not already synced by an older valid and non-deleted filter
+and not exists (select s.id from __TARGETTABLE___syncfilter s
+					inner join principaledge e on e.endvertex = s.userid and e.startprincipaltype = 1 --user
+						where s.itemid = [base].id and e.startvertex = @userid and s.appliedrules > 0 and e.IsDeleted = 0 and (s.modified <=  @sync_min_timestamp and e.modified <= @sync_min_timestamp))
+UNION ALL
+-- clause 3: get all toombstoned items
+--					- where no non-deleted syncfilter exists
+--					- and there is a deleted sync filter younger than "date"
+SELECT 	[side].[ID], 
+	[base].[Title], 
+	[base].[Description], 
+	[base].[StatusValue], 
+	[base].[EscalationLevel], 
+	[base].[Opened], 
+	[base].[Closed], 
+	[base].[TenantId], 
+	1 as [sync_row_is_tombstone], -- mark all rows that lost their sync filter as "tombstoned" so Dotmim.Sync also detects these as tombstoned/deleted
+	[side].[create_scope_id], 
+	[side].[create_timestamp], 
+	[side].[update_scope_id], 
+	[side].[update_timestamp] 
+FROM [__TARGETTABLE__] [base]
+RIGHT JOIN [__TARGETTABLE___tracking] [side]
+ON [base].[Id] = [side].[Id]
+WHERE (
+	([side].[TenantId] = @TenantId)
+	OR (([side].[update_scope_id] = @sync_scope_id or [side].[update_scope_id] IS NULL)
+		AND ([side].[TenantId] IS NULL))
+	)
+-- we do NOT care who actually made the changes, as the sync is triggered by the syncfilter table only!
+--AND (
+--	-- Update made by the local instance
+--	[side].[update_scope_id] IS NULL
+--	-- Or Update different from remote
+--	OR [side].[update_scope_id] <> @sync_scope_id
+--	-- Or we are in reinit mode so we take rows even thoses updated by the scope
+--	OR @sync_scope_is_reinit = 1
+--    )
+-- we also do NOT care if the row in the base table has changed, or it is a new scope (as if it IS a new scope, the tombstoned items should not be retreived anyways) (correct?)
+--AND (
+--	-- And Timestamp is > from remote timestamp
+--	[side].[timestamp] > @sync_min_timestamp
+--	OR
+--	-- remote instance is new, so we don't take the last timestamp
+--	@sync_scope_is_new = 1
+--	)
+AND (
+	-- we do NOT care about items here, that are deleted in the base table! (see clause 1 for these)
+	---- And the row is toombstoned
+	--[side].[sync_row_is_tombstone] = 1 
+	--OR
+	-- Or there exists a row in the base table (RIGHT JOIN!)s
+	(
+		[side].[sync_row_is_tombstone] = 0 AND [base].[Id] is not null
+		AND
+		
+		-- where no non-deleted syncfilter exists
+		not exists (select s.id from __TARGETTABLE___syncfilter s
+							inner join principaledge e on e.endvertex = s.userid and e.startprincipaltype = 1 --user
+						where s.itemid = [base].id and e.startvertex = @userid and s.appliedrules > 0 and e.isdeleted = 0)
+		-- and there is a deleted sync filter younger than "date"
+		and exists (select s.id from __TARGETTABLE___syncfilter s 
+							inner join principaledge e on e.endvertex = s.userid and e.startprincipaltype = 1 --user
+						where s.itemid = [base].id and e.startvertex = @userid and ((s.appliedrules = 0 and s.modified > @sync_min_timestamp) or (e.IsDeleted = 1 and e.modified > @sync_min_timestamp)))
+	)
+)
+
+END
+
+
+GO
+
+
